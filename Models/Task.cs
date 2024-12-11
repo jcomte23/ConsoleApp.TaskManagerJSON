3e6d7b818e2857723670ac8a78e79abbe738ddb4
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-12-11 09:06:02 -0500

    Feat: The "task" class was created and configured with its constructor

---
+++ Models/Task.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp.TaskManagerJSON.Models;
+internal class Task(string name, string description)
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Name { get; set; } = name;
+    public string Description { get; set; } = description;
+    public bool Completed { get; set; } = false;
+}
