99749306c52e837a433ae2ac6b2e96a059a4e59e
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-12-11 10:13:33 -0500

    Feat: The task listing service was created

--- Program.cs
+++ Program.cs
@@ -1,9 +1,38 @@
 ﻿using ConsoleApp.TaskManagerJSON.Models;
+using ConsoleApp.TaskManagerJSON.Services;
 
-var tarea = new PersonalTask("example", "example");
+var tasks = new List<PersonalTask>
+{
+    new PersonalTask("Estudiar C#", "Estudiar las colecciones en C#"),
+    new PersonalTask("Hacer ejercicio", "Ir al gimnasio por 1 hora"),
+    new PersonalTask("Leer libro", "Leer 20 páginas de un libro técnico")
+};
 
-Console.WriteLine(tarea.Id);
-Console.WriteLine(tarea.Name);
-Console.WriteLine(tarea.Description);
-Console.WriteLine(tarea.Completed);
-Console.WriteLine();
+var flag = true;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("####################");
+    Console.WriteLine("1. List all tasks");
+    Console.WriteLine("2. Add new task");
+    Console.WriteLine("3. Mark task as completed");
+    Console.WriteLine("4. Delete task");
+    Console.WriteLine("5. Exit");
+    Console.WriteLine("####################");
+
+    var option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            TaskDisplayService.DisplayTasksInTable(tasks);
+            break;
+        default:
+            break;
+    }
+}
+while (flag == true);
+
+
+
---
+++ Services/TaskDisplayService.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp.TaskManagerJSON.Models;
+
+namespace ConsoleApp.TaskManagerJSON.Services;
+internal class TaskDisplayService
+{
+    public static void DisplayTasksInTable(List<PersonalTask> tasks)
+    {
+        Console.Clear();
+        Console.WriteLine("{0,-40} {1,-30} {2,-50} {3,-10}", "ID", "Nombre", "Descripción", "Completada");
+        foreach (var item in tasks)
+        {
+            Console.WriteLine("{0,-40} {1,-30} {2,-50} {3,-10}", item.Id, item.Name, item.Description, item.Completed);
+        }
+        Console.WriteLine("Click to continue ...");
+        Console.ReadKey();
+    }
+}
+
